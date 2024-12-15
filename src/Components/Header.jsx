702fc1da46e5b6048c7430b85b734fe6d761e30b
Bugra Sitemkar <bugrasitemkar@gmail.com>
2024-12-15 22:51:56 +0800

    App scaffold Header menu

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from './../assets/Images/logo.png'
+
+function Header() {
+  return (
+<div className="flex items-center">
+  {/* <img src={logo} className="w-[30px] mr-4" alt="Logo" /> */}
+
+  {/* Center the ul using flex-grow or flex-1 */}
+  <ul className="flex flex-1 justify-center gap-4 md:gap-14">
+    <li className="hover:font-bold cursor-pointer">Home</li>
+    <li className="hover:font-bold cursor-pointer">About</li>
+    <li className="hover:font-bold cursor-pointer">Contact</li>
+  </ul>
+</div>
+  )
+}
+
+export default Header
